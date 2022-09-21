f216b65cb2a2075edfbce29926261512581ec176
Rio Edwards <rioredwards@gmail.com>
2022-09-20 17:25:48 -0700

    Complete renderTask

--- renderUtils.js
+++ renderUtils.js
@@ -1,8 +1,17 @@
 export function renderTask(task) {
     const liTaskContainer = document.createElement('li');
+    liTaskContainer.classList.value = 'task-card';
     const spanTaskEnergy = document.createElement('span');
+    spanTaskEnergy.id = 'task-energy';
+    spanTaskEnergy.innerHTML = task.energy;
     const imgTaskIcon = document.createElement('img');
+    imgTaskIcon.id = 'task-icon';
+    imgTaskIcon.src = `./assets/${task.icon}.png`;
+    imgTaskIcon.alt = task.icon;
     const spanTaskName = document.createElement('span');
+    spanTaskName.classList.value = 'task-name';
+    spanTaskName.id = 'task-name';
+    spanTaskName.innerHTML = task.name;
     liTaskContainer.append(spanTaskEnergy, imgTaskIcon, spanTaskName);
     return liTaskContainer;
 }
